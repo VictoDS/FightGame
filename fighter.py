d8b05815194183b55eebf90db17fa66315df6fda
Victor Scandolara <victor.scandolara@gmail.com>
2023-04-17 08:06:17 -0300

    Configuring combat and animation

--- fighter.py
+++ fighter.py
@@ -1,15 +1,32 @@
 import pygame
 
 class Fighter():
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, data, sprite_sheet, animation_steps) -> None:
+        self.size = data[0]
+        self.flip = False
         self.rect = pygame.Rect((x,y,80,180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
 
+    def load_images(self, sprite_sheet, animation_steps):
+        animation_list = []
+
+        for row, animation in enumerate(animation_steps):
+            temp_img_list = []
+
+            for step in range(animation):
+                temp_img = sprite_sheet.subsurface(step * self.size, row * self.size, self.size, self.size)
+                temp_img_list.append(temp_img)
+
+            animation_list.append(temp_img_list)
+    
     def draw(self, surface):
         pygame.draw.rect(surface, (255,0,0), self.rect)
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         floor = screen_height - (screen_height / 6)
@@ -18,14 +35,22 @@ class Fighter():
 
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-        if key[pygame.K_w] and not self.jump:
-            self.vel_y = -30
-            self.jump = True
-        
+        if not self.attacking:
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            if key[pygame.K_w] and not self.jump:
+                self.vel_y = -30
+                self.jump = True
+            
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
 
         self.vel_y += GRAVITY
         dy += self.vel_y
@@ -40,5 +65,18 @@ class Fighter():
             self.jump = False
             dy = floor - self.rect.bottom
         
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+        
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+
+        pygame.draw.rect(surface, (0,255,0), attacking_rect)
--- main.py
+++ main.py
@@ -12,22 +12,45 @@ pygame.display.set_caption("Brawler")
 clock = pygame.time.Clock()
 FPS = 60
 
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+WARRIOR_SIZE = 162
+WARRIOR_DATA = [WARRIOR_SIZE]
+WIZARD_SIZE = 250
+WIZARD_DATA = [WIZARD_SIZE]
+
 bg_image = pygame.image.load("assets\\images\\background\\background.jpg").convert_alpha()
+
+warrior_sheet = pygame.image.load("assets\\images\\warrior\\Sprites\\warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("assets\\images\\wizard\\Sprites\\wizard.png").convert_alpha()
+
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg,(0,0))
 
-fighter_1 = Fighter(200,320)
-fighter_2 = Fighter(700,320)
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
+fighter_1 = Fighter(200,320,WARRIOR_DATA,warrior_sheet,WARRIOR_ANIMATION_STEPS)
+fighter_2 = Fighter(700,320,WIZARD_DATA,wizard_sheet,WIZARD_ANIMATION_STEPS)
 
 run = True
 while run:
     clock.tick(FPS)
     
     draw_bg()
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
 
-    fighter_1.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+    fighter_1.move(SCREEN_WIDTH,SCREEN_HEIGHT,screen,fighter_2)
 
     fighter_1.draw(screen)
     fighter_2.draw(screen)
